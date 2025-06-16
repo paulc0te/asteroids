38de9d30fdda2db2e8686315989e76164473d5af
Paulc0te <alexcorpit@gmail.com>
2025-06-16 16:38:25 +0200

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pygame
 from constants import *
-# import time
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+
+        screen.fill("black")
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
