cda9c9942a0421a779b7ead62f300d7889671297
Paulc0te <alexcorpit@gmail.com>
2025-06-17 16:21:23 +0200

    CH2_Player_L3_FPS

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0      # delta time
 
+# game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +16,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
